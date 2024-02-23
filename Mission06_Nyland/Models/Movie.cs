9aee36aa18c74b9705a2bc86a998c38d981e0e97
kinseenyland <kinseenyland@gmail.com>
2024-02-23 15:23:26 -0700

    idk more changes

--- Mission06_Nyland/Models/Movie.cs
+++ Mission06_Nyland/Models/Movie.cs
@@ -10,21 +10,25 @@ namespace Mission06_Nyland.Models
         public int MovieId { get; set; }
 
         [ForeignKey("CategoryId")]
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
 
+        [Required(ErrorMessage = "Sorry, you need to enter a title")]
         public string Title { get; set; }
 
+        [Required]
         [Range(1888, 2024, ErrorMessage = "Year must be between 1888 and 2024")]
-        public int Year { get; set; }
+        public int Year { get; set; } = 0;
 
         public string? Director { get; set; }
 
         public string? Rating { get; set; }
 
+        [Required(ErrorMessage = "Sorry, you need to enter if the movie is edited or not")]
         public bool Edited { get; set; }
 
         public string? LentTo { get; set; }
 
+        [Required(ErrorMessage = "Sorry, you need to enter if the movie is copied to Plex or not")]
         public bool CopiedToPlex { get; set; }
 
         public string? Notes { get; set; }
