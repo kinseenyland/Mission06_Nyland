0b5f5e5b4cb7773e5d998b978ede1feb285f5367
kinseenyland <kinseenyland@gmail.com>
2024-02-23 14:35:31 -0700

    all the new changes ayayayay

--- Mission06_Nyland/Controllers/HomeController.cs
+++ Mission06_Nyland/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Mission06_Nyland.Models;
 using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Mission06_Nyland.Controllers
 {
@@ -25,16 +26,86 @@ namespace Mission06_Nyland.Controllers
         [HttpGet]
         public IActionResult MovieForm()
         {
-            return View();
+            ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+
+            return View("MovieForm", new Movie());
         }
 
         [HttpPost]
-        public IActionResult MovieForm(Form response)
-        { 
-            _context.Forms.Add(response);
+        public IActionResult MovieForm(Movie response)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(response); //Add record to the database
+                _context.SaveChanges();
+
+                return View("Confirmation", response);
+            }
+            else //Invalid data
+            {
+                ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+
+                return View(response);
+            }
+        }
+
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieList()
+        {
+            //Linq
+            var movies = _context.Movies
+                .OrderBy(x => x.MovieId).ToList();
+
+            return View(movies);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordToEdit = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+
+            return View("MovieForm", recordToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie updatedInfo)
+        {
+            _context.Update(updatedInfo);
             _context.SaveChanges();
 
-            return View("Index", response); 
+            return RedirectToAction("MovieList");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var recordToDelete = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            return View(recordToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Movie deletedInfo)
+        {
+            _context.Movies.Remove(deletedInfo);
+            _context.SaveChanges();
+
+            return RedirectToAction("MovieList");
         }
     }
 }
---
+++ Mission06_Nyland/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission06_Nyland.Models
+{
+    public class Movie
+    {
+        [Key]
+        [Required]
+        public int MovieId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+
+        public string Title { get; set; }
+
+        [Range(1888, 2024, ErrorMessage = "Year must be between 1888 and 2024")]
+        public int Year { get; set; }
+
+        public string? Director { get; set; }
+
+        public string? Rating { get; set; }
+
+        public bool Edited { get; set; }
+
+        public string? LentTo { get; set; }
+
+        public bool CopiedToPlex { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
